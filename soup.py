259b061cdacadaa4744c6cb8acc37f527ead263f
yaraH12 <yarahallak@gmail.com>
2023-01-23 16:51:30 +0100

    soupSemantics_inputSemanticTransitionRelation

--- soup.py
+++ soup.py
@@ -38,3 +38,19 @@ class SoupSemantics(SemanticTransitionRelation):
         return action.execute(t)
 
 
+class InputSoupSemantics(inputSemanticTransitionRelation):
+    def __init__(self,program):
+        self.program = program
+    
+    def initialConfigurations(self):
+        return [program.init]
+    
+    def enablesActions(self, input, source):
+        return filter(lambda r: r.guard(input, source), self.program.rules)
+    
+    def execute(self, action, input, source):
+        target = copy.deepcopy(source)
+        n = action(input, target)
+        return [target]
+
+
