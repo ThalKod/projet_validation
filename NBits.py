4e7a2036525b74f9b879643a11b058b8424b50ba
Bachar Hajjar <bacharhajjar2000@gmail.com>
2023-01-23 14:54:59 +0100

    NBits tests in the main. Change of Next method of NBits class

--- NBits.py
+++ NBits.py
@@ -1,34 +1,26 @@
 from model import TransitionRelation
 
 class NBits(TransitionRelation):
+
+
     def __init__(self, roots:list, n:int):
         super().__init__()
-        self.g = g
-        self.iniS = iniS
-
-    def roots(self):
-        return self.initials
-    # def next(self,source):
-    #     newV = []
-    #     res = []
-    #     n = 0
-    #     copy = source
-    #     print(copy)
-    #     for i in range(len(copy)):
-    #         for j in range(len(source)):
-    #             if (i == j):
-    #                 if (copy[i] == 0):
-    #                     n = 1
-    #                 else:
-    #                     n = 0
-    #                 newV.append(n)
-    #             else:
-    #                 newV.append(source[j])
-    #         res.append(newV)
-    #         newV = []
-    #     return res
+        self.initial = roots
+        self.nBits = n
 
     def roots(self):
         return self.initial
 
+    def next(self, source):
+        neighbours = []
+        for i in range(self.nBits):
+            neighbours.append(source ^ (1 << i))
+        return neighbours
 
+
+
+
+def binary_print(s):
+    return set(map(
+        lambda x: "{0:03b}".format(x),
+        s))
