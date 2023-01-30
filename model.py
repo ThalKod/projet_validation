3a67c4708edb3332e2c7ac0a72d75facb3bc8ed1
yaraH12 <yarahallak@gmail.com>
2023-01-30 14:09:49 +0100

    step_sync_product

--- model.py
+++ model.py
@@ -78,8 +78,13 @@ class STR2TR(TransitionRelation):
 
 class StepSynchronousProduct(SemanticTransitionRelation):
     def __init__(self,lhs,rhs):
-        pass
+        self.lhs = lhs
+        self.rhs = rhs
     def initial(self):
-        pass
+        r = []
+        for lc in lhs.initial():
+            for rc in rhs.initial():
+                r.append((lc,rc))
+        return r
     def enabledActions(self, source):
         pass
