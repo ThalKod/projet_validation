49a60833d8df8cc12220a4f8f69cb908ac402523
ThalKod <tmarcelin@thal.tech>
2023-02-06 16:37:29 +0100

    add v3 alice et bobo

---
+++ AliceBobV3.py
@@ -0,0 +1,66 @@
+from bfs import predicate_model_checker
+from model import STR2TR, IsAcceptingProxy
+from soup import SoupProgram, SoupSemantics
+
+
+class AliceBobConfiguration:
+    def __init__(self):
+        self.PC_alice = 0
+        self.PC_bob = 0
+
+    def __hash__(self):
+        return hash(self.PC_alice + self.PC_bob)
+
+    def __eq__(self, other):
+        return self.PC_alice == other.PC_alice & self.PC_bob == other.PC_bob
+
+    def __repr__(self):
+        return str(self.PC_alice) + str(self.PC_bob)
+
+
+def counterState():
+    soup = SoupProgram(AliceBobConfiguration())
+
+    def InitialToWaiting_Alice(c):
+        c.PC_alice = 1
+
+    soup.add("ItoSC_alice", lambda c: c.PC_alice == 0, InitialToWaiting_Alice)
+
+    def WaitingToCriticalSection_Alice(c):
+        return 1
+
+    soup.add("WtoSC_alice", lambda c: c.PC_bob == 0 and c.PC_alice == 1, WaitingToCriticalSection_Alice)
+
+    def CriticalSectionToInitial_Alice(c):
+        c.PC_alice = 0
+
+    soup.add("SCtoI_alice", lambda c: c.PC_alice == 1, CriticalSectionToInitial_Alice)
+
+    def InitialToWaiting_Bob(c):
+        c.PC_bob = 1
+
+    soup.add("ItoW_bob", lambda c: c.PC_bob == 0, InitialToWaiting_Bob)
+
+    def WaitingToCriticalSection_Bob(c):
+        return 1
+
+    soup.add("WtoSC_bob", lambda c: c.PC_alice == 0 and c.PC_bob == 1, WaitingToCriticalSection_Bob)
+
+    def CriticalSectionToInitial_Bob(c):
+        c.PC_bob = 0
+
+    soup.add("SCtoI_bob", lambda c: c.PC_bob == 1, CriticalSectionToInitial_Bob)
+
+    return soup
+
+
+if __name__ == '__main__':
+    semantic = SoupSemantics(counterState())
+    tr = STR2TR(semantic)
+    tr = IsAcceptingProxy(tr, lambda c: c.PC_alice == 0)
+    print(tr.initial())
+    print(tr.next(tr.initial()[0]))
+    r = predicate_model_checker(semantic, lambda c: c.PC_alice == 1 and c.PC_bob == 1)
+    print(r)
+    r = predicate_model_checker(semantic, lambda c: len(semantic.actions(c)) == 0)
+    print(r)
